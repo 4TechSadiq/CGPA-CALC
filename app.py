b644172ef027a463a3b8259cabc1cf2b4d0c05e6
C.P. Mohammed Sadiq Ali <22ad064@excelcolleges.com>
2024-05-25 00:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from cgpa import cgpa
 from image_processing import get_mark 
 import pandas as pd
 import json
+from contact import send_mail
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -29,14 +30,13 @@ def auto():
                 data_records = data.to_dict(orient="records")
                 print(f"Extracted data: {data_records}")
                 
-                # Perform CGPA calculation based on extracted data
                 data_df = pd.DataFrame(data_records)
-
                 return render_template("automatic.html", data=data_records)
             except PermissionError:
                 error = "Permission error while accessing the image."
             except Exception as e:
                 error = f"An error occurred while processing the image: {str(e)}"
+
         if 'sub-code' in request.form:
             try:
                 data = []
@@ -45,7 +45,8 @@ def auto():
                     credit = request.form.getlist('credit')[i]
                     gp = request.form.getlist('gp')[i]
                     data.append({'c_code': sub_code, 'credit': credit, 'gp': gp})
-
+                
+                print(data)
                 data_df = pd.DataFrame(data)
                 cgp = cgpa(data_df)
                 return render_template("automatic.html", cgp=cgp, data=data_df.to_dict(orient="records"))
@@ -54,14 +55,49 @@ def auto():
 
     return render_template("automatic.html", error=error, data=data, cgp=cgp)
 
-
-@app.route("/manual")
+@app.route("/manual", methods=['GET', 'POST'])
 def manual():
-    return render_template("manual.html")
+    data = []
+    cgp = None
+    error = None
 
-@app.route("/contact")
+    if request.method == "POST":
+        sub = request.form["s_code"]
+        credit = request.form["cred"]
+        mark = request.form["mark"]
+        new_entry = {"s": sub, "c": credit, "m": mark}
+
+        for entry in request.form.getlist('data'):
+            s, c, m = entry.split('|')
+            data.append({"s": s, "c": c, "m": m})
+        
+        data.append(new_entry)
+        
+        try:
+            data_df = pd.DataFrame(data)
+            cgp = cgpa(data_df)
+        except Exception as e:
+            error = f"An error occurred while processing the data: {str(e)}"
+
+    return render_template("manual.html", data=data, cgp=cgp, error=error)
+
+
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+
+    if request.method == "POST":
+        name  = request.form["user"]
+        mail = request.form["mail"]
+        message = request.form["message"]
+
+        if send_mail(name=name,mail=mail,msg=message):
+            return render_template("contact.html", msg="Thanks for giving us your valuable feedback.")
+
+
+
     return render_template("contact.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
