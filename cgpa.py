352477e08331e041abb74e7591768e6296f2b7b7
C.P. Mohammed Sadiq Ali <22ad064@excelcolleges.com>
2024-05-25 00:34:04 +0530

    Update cgpa.py

--- cgpa.py
+++ cgpa.py
@@ -2,8 +2,8 @@ import pandas as pd
 from PIL import Image
 from image_processing import get_mark
 
-# image = Image.open("test8.jpg")
-# data_frame = get_mark(image)
+image = Image.open("test8.jpg")
+data_frame = get_mark(image)
 
 def cgpa(data: pd.DataFrame):
     cred_x_mark = 0
@@ -14,12 +14,14 @@ def cgpa(data: pd.DataFrame):
                 cred = int(data.iloc[i][1])
                 mark = float(data.iloc[i][2])
             except ValueError:
+                # Skip rows with non-numeric credit or GP
                 continue
             cred_t += cred
             cred_x_mark += cred * mark
         cgpa = cred_x_mark / cred_t if cred_t != 0 else 0
-        print(cgpa)
         return cgpa
     except Exception as e:
         return f"Error: {str(e)}"
 
+# print(data_frame)
+# print(cgpa(data=data_frame))
