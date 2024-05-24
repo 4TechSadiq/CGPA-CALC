0c96821fa904605e568fbf667371b55d70ad5f8d
C.P. Mohammed Sadiq Ali <22ad064@excelcolleges.com>
2024-05-25 00:35:05 +0530

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -5,22 +5,31 @@ import pandas as pd
 MY_CONFIG = r"--psm 6 --oem 3"
 
 def get_mark(img):
-    data = pytesseract.image_to_string(img,config=MY_CONFIG)
+    data = pytesseract.image_to_string(img, config=MY_CONFIG)
     split_list = data.split("\n")
-    data_list = [split_list[i].split(" ") for i in range(0,len(split_list))]
-    
-    course_codes = [row[1] for row in data_list if len(row)>1]
-    credit = [row[-4] for row in data_list if len(row)>1]
-    gp = [row[-2] for row in data_list if len(row)>1]
+    data_list = [row.split() for row in split_list if row.strip()]
+    data_list.pop(0)
+    data_list.pop(0)
+    course_codes = []
+    credits = []
+    gps = []
+
+    for row in data_list:
+        if len(row) > 4:  
+            course_codes.append(row[1])
+            credits.append(row[-4])
+            gps.append(row[-2])
+        else:
+            print(f"Skipping row due to unexpected length: {row}")
 
     mark_dict = {
         "c_code": course_codes,
-        "credit": credit,
-        "gp": gp
+        "credit": credits,
+        "gp": gps
     }
 
     mark_data = pd.DataFrame(mark_dict)
     return mark_data
 
-# i = Image.open("test8.jpg")
+# i = Image.open("test9.jpg")
 # print(get_mark(img=i))
